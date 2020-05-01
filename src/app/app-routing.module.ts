fec2ace06a781ebc90cfb762ad7be5766e568c2a
Sean Trent <sean.d.trent@gmail.com>
2020-05-01 17:23:09 -0400

    Added comment explaining part of 'lazy-loaded' modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   //temporary homepage while testing pages
   {
+    // Page is a "lazy-loaded" module, so this is how it must be loaded to avoid duplication of child routes
     path: 'page',
     loadChildren: () => import('./page/page.module').then(m => m.PageModule)
   },
