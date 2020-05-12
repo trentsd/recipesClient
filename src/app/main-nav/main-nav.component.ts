bf4c56fcd15666bdbddd4a64967cd0239a35f1b5
Sean Trent <sean.d.trent@gmail.com>
2020-05-12 15:07:04 -0400

    Page links are now dynamically loaded into DOM using ngFor. Added rest of pages to pages.json

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { IPage } from '../shared/interfaces';
+import { DataService } from '../core/data.service';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit{
+
+  pages: IPage[];
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +20,12 @@ export class MainNavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private dataService: DataService,
+              private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit() {
+    this.dataService.getPages()
+      .subscribe((dataPages: IPage[]) => this.pages = dataPages);
+  }
 
 }
