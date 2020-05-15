7ab26703508e8ad5e6fda9870c0bfbd1d2127170
Sean Trent <sean.d.trent@gmail.com>
2020-05-15 15:49:53 -0400

    Comments are now loaded on to cards with name and content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 import { CommentsModule } from './comments/comments.module';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
--- src/app/comments/comments.module.ts
+++ src/app/comments/comments.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentsComponent } from './comments.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [CommentsComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   exports: [ CommentsComponent ]
 })
