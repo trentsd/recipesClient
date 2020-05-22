7a529d8d47abd2660400d1199b5182a82999931c
Sean Trent <sean.d.trent@gmail.com>
2020-05-22 11:56:44 -0400

    Added comment-edit for adding, editing, and deleting comments

--- src/app/comments/comments.module.ts
+++ src/app/comments/comments.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CommentsComponent } from './comments.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
+import { CommentEditComponent } from './comment-edit/comment-edit.component';
 
 @NgModule({
-  declarations: [CommentsComponent],
+  declarations: [CommentsComponent, CommentEditComponent],
   imports: [
     CommonModule,
     HttpClientModule,
--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -10,45 +10,57 @@ import { IPage, IComment } from '../shared/interfaces'
 @Injectable()
 export class DataService {
 
-    baseUrl: string = 'assets/';
+  //
+  public API = '//localhost:8080';
+  public COMMENTS_API = this.API + '/comments';
 
-    constructor(private http: HttpClient) { }
+  baseUrl: string = 'assets/';
 
-    getPages() : Observable<IPage[]> {
-      return this.http.get<IPage[]>(this.baseUrl + 'pages.json')
-        .pipe(
-          catchError(this.handleError)
+  constructor(private http: HttpClient) { }
+
+  getPages() : Observable<IPage[]> {
+    return this.http.get<IPage[]>(this.baseUrl + 'pages.json')
+          .pipe(catchError(this.handleError)
         );
     }
 
-    getPage(id: number) : Observable<IPage> {
-      return this.http.get<IPage[]>(this.baseUrl + 'pages.json')
-        .pipe(
-          map(pages => {
-            let page = pages.filter((ipage: IPage) => ipage.id === id);
-            return (page && page.length) ? page[0] : null;
-          }),
-          catchError(this.handleError)
-        )
-    }
+  getPage(id: number) : Observable<IPage> {
+    return this.http.get<IPage[]>(this.baseUrl + 'pages.json')
+      .pipe(
+        map(pages => {
+          let page = pages.filter((ipage: IPage) => ipage.id === id);
+          return (page && page.length) ? page[0] : null;
+        }),
+        catchError(this.handleError)
+      )
+  }
 
-    getAllComments(): Observable<any> {
-      return this.http.get('//localhost:8080/comments');
-    }
+  getAllComments(): Observable<any> {
+    return this.http.get(this.API + this.COMMENTS_API);
+  }
 
-    getCommentsForThisPage(id: number) : Observable<any> {
-      return this.http.get('//localhost:8080/page/' + id.toString());
-    }
+  getCommentsForThisPage(id: number) : Observable<any> {
+    return this.http.get(this.API + '/page/' + id.toString());
+  }
 
-    private handleError(error: any) {
-      console.error('server error:', error);
-      if (error.error instanceof Error) {
-          const errMessage = error.error.message;
-          return Observable.throw(errMessage);
-          // Use the following instead if using lite-server
-          // return Observable.throw(err.text() || 'backend server error');
-      }
-      return Observable.throw(error || 'Node.js server error');
+  // saveComment(comment: any): Observable<any> {
+  //   let result: Observable<any>;
+  //   if (comment['href']) {
+  //     result = this.http.put(comment.href, comment);
+  //   } else {
+  //     result = this.http.post()
+  //   }
+  // }
+
+  private handleError(error: any) {
+    console.error('server error:', error);
+    if (error.error instanceof Error) {
+      const errMessage = error.error.message;
+      return Observable.throw(errMessage);
+      // Use the following instead if using lite-server
+      // return Observable.throw(err.text() || 'backend server error');
     }
+    return Observable.throw(error || 'Node.js server error');
+  }
 
 }
