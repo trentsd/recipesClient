0c217af4b58f924c96b7baecdf60a87b9b9f61ea
Sean Trent <sean.d.trent@gmail.com>
2020-06-28 13:21:11 -0400

    Interceptor now converts http to https

--- src/app/core/http-interceptors/ensure-https-interceptor.ts
+++ src/app/core/http-interceptors/ensure-https-interceptor.ts
@@ -5,13 +5,19 @@ import {
 
 import { Observable } from 'rxjs';
 
-/** Do nothing. Testing deployment first
- * Pass untouched request through to the next request handler. */
+/** Convert http requests to https by appending the request url
+ *  Taken from Angular documentation:
+ *  https://angular.io/guide/http#intercepting-requests-and-responses
+*/
 @Injectable()
 export class EnsureHttpsInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler):
-    Observable<HttpEvent<any>> {
-    return next.handle(req);
+  intercept(req: HttpRequest<any>, next: HttpHandler) : Observable<HttpEvent<any>> {
+    // clone request and replace 'http://' with 'https://' at the same time
+    const secureReq = req.clone({
+      url: req.url.replace('http://', 'https://')
+    });
+    // send the cloned, "secure" request to the next handler.
+    return next.handle(secureReq);
   }
 }
